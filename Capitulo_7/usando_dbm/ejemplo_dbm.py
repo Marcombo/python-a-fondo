dbb9eed99e3b08acccbe359a69be2e4ae187578c
Oscar Ramirez <tuxskar@gmail.com>
2020-06-20 17:26:49 +0200

    Ejemplo usando dbm

---
+++ Capitulo_7/usando_dbm/ejemplo_dbm.py
@@ -0,0 +1,25 @@
+import dbm
+
+
+def escribe_db(nombre_db):
+    # abriendo la base de datos
+    with dbm.open(nombre_db, 'c') as db:
+        db['usuario1'] = 'Paco Lopez'
+        db['usuario1_likes'] = '34'
+        db['url'] = 'www.elpythonista.com'
+        db['titulo_libro'] = 'Python a Fondo'
+        # Comprobando los datos usando bytes
+        assert db[b'titulo_libro'] == b'Python a Fondo'
+        del db['usuario1_likes']
+
+
+def imprime_db(nombre_db):
+    with dbm.open(nombre_db, 'r') as db:
+        for clave in db.keys():
+            print(f'{clave} -> {db[clave]}')
+
+
+if __name__ == '__main__':
+nombre_db = 'dbm_db'
+escribe_db(nombre_db)
+imprime_db(nombre_db)
