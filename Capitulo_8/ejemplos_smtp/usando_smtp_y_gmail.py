df9ed4d6bda9ae52081a28d63714a1493bd3de54
Oscar Ramirez <tuxskar@gmail.com>
2020-07-01 21:21:34 +0200

    Ejemplo SMTP usando multipart

--- Capitulo_8/ejemplos_smtp/usando_smtp_y_gmail.py
+++ Capitulo_8/ejemplos_smtp/usando_smtp_y_gmail.py
@@ -12,13 +12,16 @@ if __name__ == '__main__':
     mensaje["From"] = email_emisor
     mensaje["To"] = email_receptor
 
+    texto = """Queri@ Pythonista {nombre},
+    Encuentra la mejor información sobre Python en www.elpythonista.com
+    Ejemplo basado en los ejemplos del libro Python a Fondo.
+    """
+
     html = """\
     <html>
       <body>
-        <p>Querid@ Pythonista {nombre},<br>
-           ¿Que tal?<br><br>
+        <p>Querid@ Pythonista {nombre},<br><br>
            Encuentra la mejor información sobre Python en <a href="www.elpythonista.com">El Pythonista</a><br><br>
-           
            Ejemplo basado en los ejemplos del libro <b>Python a Fondo</b>
         </p>
       </body>
@@ -26,13 +29,16 @@ if __name__ == '__main__':
     """
 
     for nombre in ['Juan', 'Maria', 'Pedro']:
+        # Creando las partes de texto y html para el email
+        mensaje_texto = MIMEText(texto.format(nombre=nombre), "plain")
         mensaje_html = MIMEText(html.format(nombre=nombre), "html")
+
+        # Agregando el contenido al mensaje multipart
+        mensaje.attach(mensaje_texto)
         mensaje.attach(mensaje_html)
 
         # Create secure connection with server and send email
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as servidor:
             servidor.login(email_emisor, password)
-            servidor.sendmail(
-                email_emisor, email_receptor, mensaje.as_string()
-            )
+            servidor.sendmail(email_emisor, email_receptor, mensaje.as_string())
