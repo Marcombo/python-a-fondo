43e000e18bf45063ca086334d6c307e0d8dc6bcc
Oscar Ramirez <tuxskar@gmail.com>
2020-07-12 12:24:18 +0200

    Eliminando contenido de web del capitulo 8

--- Capitulo_8/django_a_fondo/blog_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Post, Category
-
-admin.site.register(Category)
-admin.site.register(Post)
--- Capitulo_8/django_a_fondo/blog_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.PostListView.as_view(), name='posts_list'),
-    path('categories/', views.CategoryListView.as_view(), name='category_main'),
-]
